ef2b93bcd35b305f9dc6716fc14a90839c160409
valerijaaa <36069107+valerijaaa@users.noreply.github.com>
2019-10-12 19:12:25 +0200

    Contact form validation

---
+++ AarhusWebDevCoop/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,51 @@
+﻿using AarhusWebDevCoop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace AarhusWebDevCoop.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+
+            // Would send an email, but I do not want to save credentials here :P
+            //MailMessage message = new MailMessage();
+            //message.To.Add("username@eaaa.dk");
+            //message.Subject = model.Subject;
+            //message.From = new MailAddress(model.Email, model.Name);
+            //message.Body = model.Message;
+            //using (SmtpClient smtp = new SmtpClient())
+            //{
+            //    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            //    smtp.UseDefaultCredentials = false;
+            //    smtp.EnableSsl = true;
+            //    smtp.Host = "smtp.gmail.com";
+            //    smtp.Port = 587;
+            //    smtp.Credentials = new System.Net.NetworkCredential("username@gmail.com", "password");
+
+            //    // send mail
+            //    smtp.Send(message);
+            //}
+
+            TempData["success"] = true;
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ AarhusWebDevCoop/ViewModels/ContactForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AarhusWebDevCoop.ViewModels
+{
+    public class ContactForm
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Please enter your name")]
+        [EmailAddress(ErrorMessage = "Please enter correct email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter a subject")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Please enter a message")]
+        public string Message { get; set; }
+    }
+}
